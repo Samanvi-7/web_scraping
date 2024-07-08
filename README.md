0716f91cda37d2c09ca8747ecd87928f8b6036c7
Samanvi-7 <Thotasamanvi@gmail.com>
2024-07-08 22:40:32 +0530

    search option update

--- README.md
+++ README.md
@@ -22,27 +22,15 @@ This Python application uses Selenium to scrape product details from a website. 
     pip install selenium
     ```
 
-## Chrome WebDriver
+## Usage
 
 To run this application, you need to download and configure Chrome WebDriver, which allows Selenium to interface with the Chrome browser. Follow these steps to set it up:
 
-1. **Check your Chrome browser version:**
-   - Open Chrome and go to `chrome://settings/help`.
-   - Note down your Chrome version number.
-
-2. **Download Chrome WebDriver:**
+1. **Download Chrome WebDriver:**
    - Visit the [Chrome WebDriver download page](https://developer.chrome.com/docs/chromedriver/downloads).
    - Download the WebDriver version that matches your Chrome browser version.
 
-3. **Setup WebDriver:**
-   - Extract the downloaded WebDriver executable (`chromedriver`) to a directory.
-   - Add the directory containing `chromedriver` to your system's PATH environment variable. This step allows you to run `chromedriver` from any location in your terminal or command prompt.
-
-4. **Verify installation:**
-   - Open a terminal or command prompt.
-   - Type `chromedriver --version` and press Enter. You should see the WebDriver version printed, confirming it's correctly set up.
-
-5. **Configure in your code:**
+2. **Configure in your code:**
    - In your Python script (`main.py`), specify the path to `chromedriver` where you initialize the WebDriver:
      ```python
      path = "/path/to/chromedriver"  # Update with your actual path
@@ -50,7 +38,7 @@ To run this application, you need to download and configure Chrome WebDriver, wh
      driver = webdriver.Chrome(service=service)
      ```
 
-6. **Run your application:**
+3. **Run the application:**
    - Execute your Python script (`main.py`) to start scraping product details from the specified website using Chrome WebDriver.
 
 
--- main.py
+++ main.py
@@ -7,16 +7,30 @@ from time import sleep
 import pandas as pd 
 import os
 
-web='https://www.kindlife.in/on-the-body/skincare-en-3/eye-care-en/'
+
+search=input("Enter the product you want to search: ")
+search_format=search.replace(' ','+')
+
+base_url = 'https://www.kindlife.in/?match=all&subcats=Y&pcode_from_q=Y&pshort=Y&pfull=Y&pname=Y&pkeywords=Y&search_performed=Y'
+search_url =f'{base_url}&q={search_format}&dispatch=products.search'
+
+print(f"Constructed Search URL : {search_url}")
 
 path="/Users/samanvithota/Desktop/web_scraping/chromedriver"
 service=Service(executable_path=path)
 
-driver=webdriver.Chrome(service=service)
-driver.get(web)
+#Chrome options for headless browser
+chrome_options=webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dec-shm-usage")
 
 
-wait = WebDriverWait(driver, 10)
+driver=webdriver.Chrome(service=service,options=chrome_options)
+driver.get(search_url)
+
+
+wait = WebDriverWait(driver, 20)
 wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="page_content_"]')))
 
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -31,28 +45,29 @@ for product in products:
     try:
         title = product.find_element(By.CLASS_NAME, "product-title").text
         price = product.find_element(By.CLASS_NAME, "bk-product-list__price").text
-    
+        brand = product.find_element(By.CLASS_NAME, "bk-product-list__feature").text
         # Split the price text
         price_details = price.split("\n")
         if len(price_details) == 3:
-            discounted_price = price_details[0]
+            sale_price = price_details[0]
             original_price = price_details[1]
             discount = price_details[2]
         elif len(price_details) == 2:
-            discounted_price = price_details[0]
+            sale_price = price_details[0]
             original_price = price_details[1]
             discount = "N/A"
         else:
-            discounted_price = price_details[0]
+            sale_price = price_details[0]
             original_price = "N/A"
             discount = "N/A"
         
-        print(f"Product name: {title}\n Original Price: {original_price}\n Discounted Price: {discounted_price}\n Discount: {discount}\n ")
+        print(f" Brand: {brand}\n Product name: {title}\n Original Price: {original_price}\n Sale Price: {sale_price}\n Discount: {discount}\n ")
 
         product_data.append({
+            'Brand': brand,
             'Title': title,
             'Original_price': original_price,
-            'Discounted_price': discounted_price,
+            'Sale_price': sale_price,
             'Discount': discount
         })
 
